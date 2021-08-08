769faa0dad71c82c2e94eb064939c36ad8293f91
Bruce McIntosh <bruce.e.mcintosh@gmail.com>
2021-08-08 16:21:32 +1000

    Updated bin programs and readme

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 Parser for go games in SGF  (Smart Game Format) Files
+that can be used to render latex igo diagrams or to underpin a GUI application.
 
-cargo build
+Build and run unit tests
+- cargo build
+- cargo test
 
-cargo test
 
-cargo run -- -f ./resource/resources/The_59th_Judan_Title_Match_3rd_game.sgf
+Parse SGF file and write latex igo markup:
+- cargo run --bin to_latex -- -f resources/The_59th_Judan_Title_Match_3rd_game.sgf
 
-cargo run --example gui -- -f resources/The_59th_Judan_Title_Match_3rd_game.sgf
+Parse SGF file and final board position in ASCII:
+  cargo run --bin to_ascii -- -f ./resources/game.sgf
+
+
+Parse SGF file and show in an example Open GL GUI:
+- cargo run --example gui -- -f resources/The_59th_Judan_Title_Match_3rd_game.sgf
---
+++ src/bin/to_latex.rs
@@ -0,0 +1,26 @@
+use clap::{App, Arg};
+
+use go;
+use anyhow::Result;
+use go::GoError;
+
+fn main() -> Result<(), GoError> {
+    let matches = App::new("sgf_to_latex")
+        .version("0.1.0")
+        .author("Bruce McIntosh <bruce.e.mcintosh@gmail.com>")
+        .about("Convert SGF file to latex")
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .takes_value(true)
+                .help("SGF file name"),
+        )
+        .get_matches();
+
+    let file_name = matches.value_of("file").map(|f| f.to_string()).ok_or(GoError::Other("No file parameter".to_string()))?;
+    let game = go::Game::from_sgf_file(file_name)?;
+    println!("{}", game.render_to_latex(50)?);
+    Ok(())
+}
+
